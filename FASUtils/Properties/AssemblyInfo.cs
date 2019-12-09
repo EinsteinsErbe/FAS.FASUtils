be8e837ce69296c91acf46bb1406de9a26b8fe4a
Manuel Jordan <manuel.jordan@ch.abb.com>
2019-12-09 11:21:02 +0100

    Update AssemblyInfo.cs

--- FASUtils/Properties/AssemblyInfo.cs
+++ FASUtils/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FAS.FASUtils")]
+[assembly: AssemblyTitle("FASUtils")]
 [assembly: AssemblyDescription("A collection of functions for the FAS team (e.g. NI SystemLink API)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Manuel Jordan")]
